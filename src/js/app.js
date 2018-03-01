643ad22e57053ea4cdddd605e1a63702e45b8445
dfesenko <dumox@online.ua>
2018-03-01 15:11:38 +0200

    add slider button and functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,16 @@
-import {HammerSlider} from 'hammer-slider';
+const HammerSlider = require('hammer-slider');
 
-const slider = HammerSlider(document.getElementById('hammer-slider'), {
-  slideSpeed: 60,
-  slideShow: true,
-  slideInterval: 4000,
-  dots: true,
-  mouseDrag: true,
-  beforeSlideChange: (targetSlide) => {
-	console.log('Changing to slide number: ' + targetSlide);
-  }
-});
+var slider = HammerSlider(document.getElementById('hammer-slider'));
+document.querySelector('.prev-feedback').addEventListener('click', slider.prev, false);
+document.querySelector('.next-feedback').addEventListener('click', slider.next, false);
+
+// const slider = HammerSlider(document.getElementById('hammer-slider'), {
+//   slideSpeed: 60,
+//   slideShow: true,
+//   slideInterval: 4000,
+//   dots: true,
+//   mouseDrag: true,
+//   beforeSlideChange: (targetSlide) => {
+// 	console.log('Changing to slide number: ' + targetSlide);
+//   }
+// });
