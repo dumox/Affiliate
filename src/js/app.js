adef5f1409261a86603a9520bb42af596248c884
dfesenko <dumox@online.ua>
2018-03-02 20:37:28 +0200

    minor changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,7 @@
 const HammerSlider = require('hammer-slider');
 
-var slider = HammerSlider(document.getElementById('hammer-slider'));
-document.querySelector('.prev-feedback').addEventListener('click', slider.prev, false);
-document.querySelector('.next-feedback').addEventListener('click', slider.next, false);
-
-// const slider = HammerSlider(document.getElementById('hammer-slider'), {
-//   slideSpeed: 60,
-//   slideShow: true,
-//   slideInterval: 4000,
-//   dots: true,
-//   mouseDrag: true,
-//   beforeSlideChange: (targetSlide) => {
-// 	console.log('Changing to slide number: ' + targetSlide);
-//   }
-// });
+if(HammerSlider) {
+  const slider = HammerSlider(document.getElementById('hammer-slider'));
+  document.querySelector('.prev-feedback').addEventListener('click', slider.prev, false);
+  document.querySelector('.next-feedback').addEventListener('click', slider.next, false);
+}
