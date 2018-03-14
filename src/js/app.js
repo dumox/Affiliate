7573d55c9581e4cf4e4cc3d01b148c5adc40dad5
dfesenko <dumox@online.ua>
2018-03-14 15:02:09 +0200

    add input effects

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+// HAMMER SLIDER
+
 const HammerSlider = require('hammer-slider');
 
 if(HammerSlider) {
@@ -5,3 +7,29 @@ if(HammerSlider) {
   document.querySelector('.btn-prev-feedback').addEventListener('click', slider.prev, false);
   document.querySelector('.btn-next-feedback').addEventListener('click', slider.next, false);
 }
+
+
+// ADD AND REMOVE CLASS TO INPUT PARENT ELEMENT 
+
+(function() {
+
+  const inputsArr = Array.from(document.querySelectorAll('input'));
+  
+  inputsArr.forEach(input => {
+    if(input.getAttribute('type') !== 'checkbox') {
+      input.addEventListener('focus', onInputFocus);
+      input.addEventListener('blur', onInputBlur);
+    }
+  });
+  
+  function onInputFocus(e) {
+    e.target.parentNode.classList.add( 'input--filled' );
+  }
+
+  function onInputBlur(e) {
+    if(e.target.value.trim() === '' ) {
+      e.target.parentNode.classList.remove( 'input--filled' );
+    }
+  }
+
+})();
